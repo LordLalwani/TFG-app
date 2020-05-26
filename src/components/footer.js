37398a6e89818a6e4dbfe184f75b2cce6fb1b79a
LordLalwani <Deepplalwani@hotmail.com>
2020-05-27 08:49:30 +1000

    Implemented header to fade in and out on scroll  - need to make smooth and make coder nicer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,24 +4,24 @@ import "./footer.scss";
 
 const Footer = () => {
     return (
-            <footer className="footer">
-                <section className="footer-container">
-                    <img src={logo} className="footer-img"></img>
+        <footer className="footer">
+            <section className="footer-container">
+                <img src={logo} alt="" className="footer-img"></img>
+                <div>
+                    <a className="footer-menu-item" href="/"><span>Home</span></a>
+                    <a className="footer-menu-item" href="/"><span>Our Work</span></a>
+                    <a className="footer-menu-item" href="/"><span>Services</span></a>
+                    <a className="footer-menu-item" href="/"><span>Contact Us</span></a>
+                </div>
+                <div className="footer-contact">
+                    <a href="mailto:Hello@thefearless.com" class="footer-email">Hello@thefearless.com</a>
                     <div>
-                        <a className="footer-menu-item" href="/"><span>Home</span></a>
-                        <a className="footer-menu-item" href="/"><span>Our Work</span></a>
-                        <a className="footer-menu-item" href="/"><span>Services</span></a>
-                        <a className="footer-menu-item" href="/"><span>Contact Us</span></a>
+                        <span className="footer-text">0800123123</span>
+                        <span className="footer-copyright footer-text">© {new Date().getFullYear()} The Fearless Creative Ltd.</span>
                     </div>
-                    <div className="footer-contact">
-                        <a href="mailto:Hello@thefearless.com" class="footer-email">Hello@thefearless.com</a>
-                        <div>
-                            <span className="footer-text">0800123123</span>
-                            <span className="footer-copyright footer-text">© {new Date().getFullYear()} The Fearless Creative Ltd.</span>
-                        </div>
-                    </div>
-                </section>
-            </footer>
+                </div>
+            </section>
+        </footer>
     )
 }
 export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,59 @@ import * as logo from "../images/fearless-logo.svg"
 import HamburgerMenu from "../components/hamburgerMenu"
 import "./header.scss"
 
+const classes = {
+  pinned: "header-pin",
+  unpinned: "header-unpin",
+}
+
+var lastKnownScrollY = 0
+var currentScrollY = 0
+var ticking = false
+var eleHeader = null
+
+window.onload = function() {
+  eleHeader = document.getElementsByClassName("header-main")
+  document.addEventListener("scroll", onScroll, false)
+}
+var lastKnownScrollY = 0
+function onScroll() {
+  const currentScrollY = window.pageYOffset
+  if(currentScrollY <= 80){
+    console.log(currentScrollY)
+    eleHeader[0].classList.remove(classes.unpinned)
+    eleHeader[0].classList.remove(classes.pinned)
+    console.log(eleHeader[0].className)
+  }
+
+  console.log(currentScrollY)
+  if (currentScrollY >= 80 && currentScrollY < lastKnownScrollY) {
+    pin()
+  } else if (currentScrollY >= 80 && currentScrollY > lastKnownScrollY) {
+    unpin()
+  }
+  lastKnownScrollY = currentScrollY
+}
+
+function pin() {
+  if (eleHeader[0].classList.contains(classes.unpinned)) {
+    eleHeader[0].classList.remove(classes.unpinned)
+    eleHeader[0].classList.add(classes.pinned)
+  }
+  console.log(eleHeader[0].className)
+}
+function unpin() {
+  if (
+    eleHeader[0].classList.contains(classes.pinned) ||
+    !eleHeader[0].classList.contains(classes.unpinned)
+  ) {
+    eleHeader[0].classList.remove(classes.pinned)
+    eleHeader[0].classList.add(classes.unpinned)
+  }
+  console.log(eleHeader[0].className)
+}
+
 const Header = ({ siteTitle }) => (
-  <header className="header-main">
+  <header id="header-id" className="header-main">
     <div className="header-container">
       <Link to="/">
         <img src={logo} alt="" className="header-img" />
