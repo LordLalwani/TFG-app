dc47092a275f5a8835da937006c0d6e8be73c4a3
LordLalwani <Deepplalwani@hotmail.com>
2020-06-15 19:34:47 +1000

    Placeholder our work page

--- src/pages/our-work-page.js
+++ src/pages/our-work-page.js
@@ -1,13 +1,24 @@
-import React from "react"
+import React, { Component } from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import "./our-work-page.scss"
 import withThemeContext from "../utils/withThemeContext"
 
-const OurWorkPage = () => (
-        <Layout>
-            <SEO title="Home" />
-            <h1>Hello From the our work page</h1>
-        </Layout>
-)
+class OurWorkPage extends Component {
+
+    render() {
+        return (
+            <Layout>
+                <section>
+                    <div className="header-section">
+                        <h1>Our Recent work</h1>
+                    </div>
+                </section>
+                <section className="work-section">
+                    
+                </section>
+            </Layout>
+        )
+    }
+}
 
 export default withThemeContext(OurWorkPage)
