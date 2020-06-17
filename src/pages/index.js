46ef080d35b2d1944a9c348e183f30728402e756
LordLalwani <Deepplalwani@hotmail.com>
2020-06-17 19:38:09 +1000

    Placeholder index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import React from "react"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import "./index.scss"
 import withThemeContext from "../utils/withThemeContext"
 
 const IndexPage = () => (
-    <Layout>
-      <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-    </Layout>
+  <Layout>
+    <SEO title="Home" />
+    <section className="hero-section">
+      <h1>Your Next Digital Innovations Partner.</h1>
+      <h3>We design, build and launch <u>user experiences.</u></h3>
+    </section>
+  </Layout>
 )
 
 export default withThemeContext(IndexPage)
