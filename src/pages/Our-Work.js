2e239ffd400eaf2f03509f19317b5c9d025f77e9
LordLalwani <Deepplalwani@hotmail.com>
2020-06-26 20:46:02 +1000

    Added navigation parity between pages

--- src/pages/Our-Work.js
+++ src/pages/Our-Work.js
@@ -2,9 +2,17 @@ import React, { Component } from "react"
 import Layout from "../components/layout"
 import "./our-work-page.scss"
 import withThemeContext from "../utils/withThemeContext"
+import { ThemeContext } from "../contexts/themeContext";
 import SEO from "../components/seo"
 
 class OurWorkPage extends Component {
+
+    static contextType = ThemeContext;
+
+    componentDidMount() {
+        const { toggleThemeIsLight } = this.context;
+        toggleThemeIsLight(true)
+    }
 
     render() {
         return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,31 @@
-import React from "react"
+import React, { Component } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "./index.scss"
 import withThemeContext from "../utils/withThemeContext"
+import { ThemeContext } from "../contexts/themeContext";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home"
-      description="The Fearless is a Creative Agency based in Melbourne specialised in creating breathtaking user expereinces through digital engineering solutions." />
-    <section className="hero-section">
-      <h1>Your Next Digital Innovations Partner.</h1>
-      <h3>We design, build and launch user experiences.</h3>
-    </section>
-  </Layout>
-)
+class IndexPage extends Component {
+
+  static contextType = ThemeContext;
+
+  componentDidMount() {
+    const { toggleThemeIsLight } = this.context;
+    toggleThemeIsLight(true)
+  }
+  
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home"
+          description="The Fearless is a Creative Agency based in Melbourne specialised in creating breathtaking user expereinces through digital engineering solutions." />
+        <section className="hero-section">
+          <h1>Your Next Digital Innovations Partner.</h1>
+          <h3>We design, build and launch user experiences.</h3>
+        </section>
+      </Layout>
+    )
+  }
+}
 
 export default withThemeContext(IndexPage)
